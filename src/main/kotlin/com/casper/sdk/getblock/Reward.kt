8a91d27c0279794b145911eb4d8fd42d181ca66c
hienbui9999 <buiduyhien@gmail.com>
2022-04-15 11:56:04 +0700

    Implementation for GetBlock RPC call

--- src/main/kotlin/com/casper/sdk/getblock/Reward.kt
+++ src/main/kotlin/com/casper/sdk/getblock/Reward.kt
@@ -1,6 +1,17 @@
 package com.casper.sdk.getblock
 
+import net.jemzart.jsonkraken.toJsonString
+import net.jemzart.jsonkraken.values.JsonObject
+
 class Reward {
     var validator:String = ""
     var amount:ULong = 0u
+    companion object {
+        fun fromJsonToReward(from:JsonObject):Reward {
+            var ret:Reward = Reward()
+            ret.validator = from["validator"].toString()
+            ret.amount = from["amount"].toJsonString().toULong()
+            return ret
+        }
+    }
 }

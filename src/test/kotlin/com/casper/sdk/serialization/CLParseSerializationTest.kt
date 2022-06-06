f805aa5448001c7a32b910a9b650edbf5a038e99
hienbui9999 <buiduyhien@gmail.com>
2022-06-06 11:59:28 +0700

    M3 implementation

--- src/test/kotlin/com/casper/sdk/serialization/CLParseSerializationTest.kt
+++ src/test/kotlin/com/casper/sdk/serialization/CLParseSerializationTest.kt
@@ -71,5 +71,52 @@ internal class CLParseSerializationTest {
         assert(CLParseSerialization.serializeFromCLParse(clParse) == "90655400")
         clParse.itsValueInStr = "0"
         assert(CLParseSerialization.serializeFromCLParse(clParse) == "00000000")
+        clParse.itsValueInStr = "334455"
+        assert(CLParseSerialization.serializeFromCLParse(clParse) == "771a0500")
+        clParse.itsValueInStr = "4099"
+        assert(CLParseSerialization.serializeFromCLParse(clParse) == "03100000")
+
+        //U64 assertion
+        clParse.itsCLType.itsTypeStr = ConstValues.CLTYPE_U64
+        clParse.itsValueInStr = "1024"
+        assert(CLParseSerialization.serializeFromCLParse(clParse) == "0004000000000000")
+        clParse.itsValueInStr = "33009900995531024"
+        assert(CLParseSerialization.serializeFromCLParse(clParse) == "10d1e87e54467500")
+        clParse.itsValueInStr = "0"
+        assert(CLParseSerialization.serializeFromCLParse(clParse) == "0000000000000000")
+        clParse.itsValueInStr = "300000"
+        assert(CLParseSerialization.serializeFromCLParse(clParse) == "e093040000000000")
+        clParse.itsValueInStr = "123456789"
+        assert(CLParseSerialization.serializeFromCLParse(clParse) == "15cd5b0700000000")
+
+        //U128 assertion
+        clParse.itsCLType.itsTypeStr = ConstValues.CLTYPE_U128
+        clParse.itsValueInStr = "123456789101112131415"
+        assert(CLParseSerialization.serializeFromCLParse(clParse) == "0957ff1ada959f4eb106")
+        clParse.itsValueInStr = "1024"
+        assert(CLParseSerialization.serializeFromCLParse(clParse) == "020004")
+        clParse.itsValueInStr = "0"
+        assert(CLParseSerialization.serializeFromCLParse(clParse) == "00")
+
+        //U256 assertion
+        clParse.itsCLType.itsTypeStr = ConstValues.CLTYPE_U256
+        clParse.itsValueInStr = "999988887777666655556666777888999"
+        assert(CLParseSerialization.serializeFromCLParse(clParse) == "0ee76837d2ca215879f7bc5ca24d31")
+        clParse.itsValueInStr = "2048"
+        assert(CLParseSerialization.serializeFromCLParse(clParse) == "020008")
+        clParse.itsValueInStr = "0"
+        assert(CLParseSerialization.serializeFromCLParse(clParse) == "00")
+
+        //U512 assertion
+        clParse.itsCLType.itsTypeStr = ConstValues.CLTYPE_U512
+        clParse.itsValueInStr = "999888666555444999887988887777666655556666777888999666999"
+        assert(CLParseSerialization.serializeFromCLParse(clParse) == "1837f578fca55492f299ea354eaca52b6e9de47d592453c728")
+        clParse.itsValueInStr = "4096"
+        assert(CLParseSerialization.serializeFromCLParse(clParse) == "020010")
+        clParse.itsValueInStr = "0"
+        assert(CLParseSerialization.serializeFromCLParse(clParse) == "00")
+        clParse.itsValueInStr = "100000000"
+        assert(CLParseSerialization.serializeFromCLParse(clParse) == "0400e1f505")
+
     }
 }
